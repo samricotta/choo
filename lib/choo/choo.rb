2b6a19f1b070f15a669ddf9ad86b6a8f39fff5a5
Sam Ricotta <samanthalricotta@gmail.com>
2019-11-06 11:27:33 +0000

    removal of discount

--- lib/choo/choo.rb
+++ lib/choo/choo.rb
@@ -1,8 +1,38 @@
-require "choo/choo/version"
+require "../choo/choo/version"
 
-module Choo
-  module Choo
-    class Error < StandardError; end
-    # Your code goes here...
-  end
+module  Choo
+  # module Choo
+    # class Error < StandardError; end
+    class Train
+      def self.choo
+        first_loop
+      end
+
+      def self.first_loop
+        string = " "
+        front = "🚂"
+        carriage = " 🚃 "
+        all_c = " 🚃 🚃 🚃 🚃 🚃"
+        print string.insert(-1, front)
+          sleep(0.5)
+        loop do
+          print string
+          string.insert(-1, carriage)
+          sleep(0.5)
+          string.insert(-1, carriage)
+          sleep(0.5)
+        end
+        # all.slice!(0)
+        # p all
+        # end
+      end
+      choo
+    end
+  # end
 end
+
+
+ # 1.upto(6) do |i|
+ #            print front
+ #            sleep(0.5)
+ #            p
